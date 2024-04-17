d970a27f42c807517037858bf1ca54c3be073c04
CharlesLindberg <charleslindberg91@gmail.com>
2024-04-17 16:05:53 +0200

    Lite av varje

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ let selectedPokemon2 = null;
 let selectedPokemons = [];
 let progressElement = document.querySelectorAll("progress");
 let currentPokemons;
+let statCountP1 = 0;
+let statCountP2 = 0;
+let battleField = document.querySelector(".battleField");
 
 // Hämta Pokémon namn till dropdown menyerna
 async function fetchPokemons(url, dropdown, count) {
@@ -80,7 +83,10 @@ function createPokemonInstance(data) {
   return new Pokémon(
     data.name,
     // data.sprites.front_default,
-    data.sprites.other.dream_world.front_default,
+    // data.sprites.other.dream_world.front_default,
+    data.sprites.other.home.front_shiny,
+    // data.sprites.other.showdown.front_default,
+    // data.sprites.other.showdown.front_shiny,
     data.types.map((type) => type.type.name),
     data.weight,
     data.height,
@@ -93,11 +99,6 @@ function createPokemonInstance(data) {
       Speed: data.stats[5].base_stat,
     }
   );
-  // pokemonInstance.progressBars = createProgressBarElements(
-  //   pokemonInstance.stats
-  // );
-
-  // return pokemonInstance;
 }
 
 // |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -150,28 +151,20 @@ function displayPokemonDetails(pokemonInstance) {
   heightPara.classList.add("height");
   pokemon1Container.append(heightPara);
 
-  //Lägg till Stats & skapa progressbar-element
+  //Lägg till Stats & skapa progressbar-element SKAPA PROGRESSBAR ELEMENT FÖR FÖRSTA POKEMONEN
   let statsList = document.createElement("ul");
   for (let stat in pokemonInstance.stats) {
     let statItem = document.createElement("li");
     let progress = document.createElement("progress");
     progress.value = pokemonInstance.stats[stat];
-    progress.max = 255;
-    progress.classList.add("progress");
+    progress.max = 160;
+    // progress.classList.add("progress");
+    progress.classList.add(`${stat}1`);
     statItem.textContent = `${stat}: ${pokemonInstance.stats[stat]}`;
     statItem.append(progress);
     statsList.append(statItem);
   }
 
-  // GAMMAL EJ FUNGERANDE KOD. RADERA
-  // let statsList = document.createElement("ul");
-  // for (let stat in pokemonInstance.stats) {
-  //   let statItem = document.createElement("li");
-  //   let progress = pokemonInstance.progressBars[stat.toUpperCase()];
-  //   statItem.textContent = `${stat}: ${pokemonInstance.stats[stat]}`;
-  //   statItem.append(progress);
-  //   statsList.append(statItem);
-  // }
   pokemon1Container.append(statsList);
 }
 
@@ -217,13 +210,15 @@ function displayPokemonDetailsForSecondContainer(pokemonInstance) {
   heightPara.classList.add("height");
   pokemon2Container.append(heightPara);
 
+  // SKAPA PROGRESSBAR ELEMENT FÖR POKEMON2-----
   let statsList = document.createElement("ul");
   for (let stat in pokemonInstance.stats) {
     let statItem = document.createElement("li");
     let progress = document.createElement("progress");
     progress.value = pokemonInstance.stats[stat];
-    progress.max = 255;
-    progress.classList.add("progress");
+    progress.max = 160;
+    // progress.classList.add("progress");
+    progress.classList.add(`${stat}2`);
     statItem.textContent = `${stat}: ${pokemonInstance.stats[stat]}`;
     statItem.append(progress);
     statsList.append(statItem);
@@ -252,6 +247,19 @@ function displayPokemonDetailsForSecondContainer(pokemonInstance) {
 // Funktion för att jämföra Pokémon mot varandra
 function comparePokemons(pokemon1, pokemon2) {
   console.log(pokemon1, pokemon2);
+
+  let p1 = [];
+  let p2 = [];
+
+  p1.push(pokemon1.stats);
+  p2.push(pokemon2.stats);
+
+  let name1 = [];
+  let name2 = [];
+
+  name1.push(pokemon1.name);
+  name2.push(pokemon2.name);
+
   if (pokemon1.weight > pokemon2.weight) {
     pokemon1Container.querySelector(".weight").classList.add("green");
     pokemon2Container.querySelector(".weight").classList.add("red");
@@ -274,115 +282,207 @@ function comparePokemons(pokemon1, pokemon2) {
     pokemon2Container.querySelector(".height").classList.add("green");
   }
 
-  // GAMMAL ONÖDIG KOD
-  // let progressBarStats1 = createProgressBarElements(pokemon1.stats);
-  // let createProgressBarStats2 = createProgressBarElements(pokemon2.stats);
+  // for (let stat in pokemon1.stats) {
+  //   let statValue1 = pokemon1.stats[stat];
+  //   let statValue2 = pokemon2.stats[stat];
 
-  // let progressBars1 = pokemon1Container.querySelectorAll("progress");
-  // let progressBars2 = pokemon2Container.querySelectorAll("progress");
+  //   console.log("Stat: ", stat);
+  //   console.log("statValue1: ", statValue1);
+  //   console.log("statValue2: ", statValue2);
 
-  for (let stat in pokemon1.stats) {
-    let statValue1 = pokemon1.stats[stat];
-    let statValue2 = pokemon2.stats[stat];
+  //   let progressBar1 = pokemon1Container.querySelectorAll(".progress");
+  //   let progressBar2 = pokemon2Container.querySelectorAll(".progress");
 
-    console.log("Stat: ", stat);
-    console.log("statValue1: ", statValue1);
-    console.log("statValue2: ", statValue2);
+  //   let stat1GreaterThanStat2 = statValue1 > statValue2;
+  //   let stat1LessThanStat2 = statValue1 < statValue2;
 
-    let progressBar1 = pokemon1Container.querySelectorAll(".progress");
-    let progressBar2 = pokemon2Container.querySelectorAll(".progress");
+  //   for (let i = 0; i < progressBar1.length; i++) {
+  //     let progressBarStat1 = progressBar1[i];
+  //     let progressBarStat2 = progressBar2[i];
 
-    let stat1GreaterThanStat2 = statValue1 > statValue2;
-    let stat1LessThanStat2 = statValue1 < statValue2;
+  //     if (stat1GreaterThanStat2) {
+  //       progressBarStat1.classList.add("greenProgress");
+  //       progressBarStat2.classList.add("redProgress");
+  //     } else if (stat1LessThanStat2) {
+  //       progressBarStat1.classList.add("redProgress");
+  //       progressBarStat2.classList.add("greenProgress");
+  //     } else {
+  //       progressBarStat1.classList.add("greenProgress");
+  //       progressBarStat2.classList.add("greenProgress");
+  //     }
+  //     // console.log("progressBarStat1: ", progressBarStat1);
+  //   }
 
-    // Loopa genom progress-elementen och rensa befintliga klasser
-    // for (let progressBarStat1 of progressBar1) {
-    //   progressBarStat1.classList.remove("redProgress", "greenProgress");
-    // }
-    // for (let progressBarStat2 of progressBar2) {
-    //   progressBarStat2.classList.remove("redProgress", "greenProgress");
-    // }
-
-    for (let i = 0; i < progressBar1.length; i++) {
-      let progressBarStat1 = progressBar1[i];
-      let progressBarStat2 = progressBar2[i];
-
-      if (stat1GreaterThanStat2) {
-        progressBarStat1.classList.add("greenProgress");
-        progressBarStat2.classList.add("redProgress");
-      } else if (stat1LessThanStat2) {
-        progressBarStat1.classList.add("redProgress");
-        progressBarStat2.classList.add("greenProgress");
-      } else {
-        progressBarStat1.classList.add("greenProgress");
-        progressBarStat2.classList.add("greenProgress");
-      }
-      // console.log("progressBarStat1: ", progressBarStat1);
-    }
-
-    // for (let i = 0; i < progressBar1.length; i++) {
-    //   let innerStatValue1 = pokemon1.stats[i];
-    //   let innerStatValue2 = pokemon2.stats[i];
-    //   let progressBarStat1 = progressBar1[i];
-    //   let progressBarStat2 = progressBar2[i];
-
-    //   if (innerStatValue1 > innerStatValue2) {
-    //     progressBarStat1.classList.add("green");
-    //     progressBarStat2.classList.add("red");
-    //   } else if (innerStatValue1 < innerStatValue2) {
-    //     progressBarStat1.classList.add("red");
-    //     progressBarStat2.classList.add("green");
-    //   } else if (innerStatValue1 === innerStatValue2) {
-    //     progressBarStat1.classList.add("green");
-    //     progressBarStat2.classList.add("green");
-    //   }
-    // }
-
-    // let progressBarPokemon1 = progressBar1[stat];
-    // let progressBarPokemon2 = progressBar2[stat];
-
-    // let progressBar1 = pokemon1Container.querySelector(
-    //   `.${stat.toLowerCase()}ProgressBar`
-    // );
-    // let progressBar2 = pokemon2Container.querySelector(
-    //   `.${stat.toLowerCase()}ProgressBar`
-    // );
-
-    // let progressBar1 = pokemon1.progressBars[stat.toUpperCase()];
-    // let progressBar2 = pokemon2.progressBars[stat.toUpperCase()];
-
-    // let progressBar1 = pokemon1Container.querySelectorAll("progress");
-    // let progressBar2 = pokemon2Container.querySelectorAll("progress");
-
-    // if (statValue1 > statValue2) {
-    //   statValue1.classList.add("green");
-    //   statValue2.classList.add("red");
-    // } else if (statValue1 < statValue2) {
-    //   statValue1.classList.add("red");
-    //   statValue2.classList.add("green");
-    // } else {
-    //   statValue1.classList.add("green");
-    //   statValue2.classList.add("green");
-    // }
-
-    // if (statValue1 > statValue2) {
-    //   progressBar1.classList.add("green");
-    //   progressBar2.classList.add("red");
-    // } else if (statValue1 < statValue2) {
-    //   progressBar1.classList.add("red");
-    //   progressBar2.classList.add("green");
-    // } else {
-    //   progressBar1.classList.add("green");
-    //   progressBar2.classList.add("green");
-    // }
-  }
+  // }
+  comparePokemonList(p1, p2);
+  comparePokemonList(name1, name2);
 }
 
-// Brandon tillägg
-// compare.addEventListener("click", () => {
-//   comparePokemons(selectedPokemon1, selectedPokemon2);
-// });
+// console.log(selectedPokemons);
 
-console.log(selectedPokemons);
-// console.log("Progress bar 1: ", progressBar1);
-// console.log("Progress bar 2: ", progressBar2);
+function comparePokemonList(p1, p2) {
+  console.log("p1", p1[0]);
+  console.log("p2", p2[0]);
+
+  let stats1 = p1[0];
+  let stats2 = p2[0];
+
+  console.log("P1 HP: ", p1[0].HP);
+  console.log("P1 Attack: ", p1[0].Attack);
+
+  console.log("P1: ", p1);
+  console.log("P2: ", p2);
+
+  // let progressBar1 = pokemon1Container.querySelectorAll(".progress");
+  // let progressBar2 = pokemon2Container.querySelectorAll(".progress");
+
+  let p1HP = document.querySelector(".HP1");
+  let p2HP = document.querySelector(".HP2");
+
+  let p1Attack = document.querySelector(".Attack1");
+  let p2Attack = document.querySelector(".Attack2");
+
+  let p1Defense = document.querySelector(".Defense1");
+  let p2Defense = document.querySelector(".Defense2");
+
+  if (p1[0].HP > p2[0].HP) {
+    p1HP.classList.add("greenProgress");
+    p2HP.classList.add("redProgress");
+    statCountP1++;
+  } else if (p1[0].HP < p2[0].HP) {
+    p1HP.classList.add("redProgress");
+    p2HP.classList.add("greenProgress");
+    statCountP2++;
+  } else {
+    p1HP.classList.add("orangeProgress");
+    p2HP.classList.add("orangeProgress");
+  }
+
+  if (p1[0].Attack > p2[0].Attack) {
+    p1Attack.classList.add("greenProgress");
+    p2Attack.classList.add("redProgress");
+    statCountP1++;
+  } else if (p1[0].Attack < p2[0].Attack) {
+    p1Attack.classList.add("redProgress");
+    p2Attack.classList.add("greenProgress");
+    statCountP2++;
+  } else {
+    p1Attack.classList.add("orangeProgress");
+    p2Attack.classList.add("orangeProgress");
+  }
+
+  if (p1[0].Defense > p2[0].Defense) {
+    p1Defense.classList.add("greenProgress");
+    p2Defense.classList.add("redProgress");
+    statCountP1++;
+  } else if (p1[0].Defense < p2[0].Defense) {
+    p1Defense.classList.add("redProgress");
+    p2Defense.classList.add("greenProgress");
+    statCountP2++;
+  } else {
+    p1Defense.classList.add("orangeProgress");
+    p2Defense.classList.add("orangeProgress");
+  }
+
+  let p1SpecialAttack = document.querySelector(".SpecialAttack1");
+  let p2SpecialAttack = document.querySelector(".SpecialAttack2");
+
+  if (p1[0].SpecialAttack > p2[0].SpecialAttack) {
+    p1SpecialAttack.classList.add("greenProgress");
+    p2SpecialAttack.classList.add("redProgress");
+    statCountP1++;
+  } else if (p1[0].SpecialAttack < p2[0].SpecialAttack) {
+    p1SpecialAttack.classList.add("redProgress");
+    p2SpecialAttack.classList.add("greenProgress");
+    statCountP2++;
+  } else {
+    p1SpecialAttack.classList.add("orangeProgress");
+    p2SpecialAttack.classList.add("orangeProgress");
+  }
+
+  let p1SpecialDefense = document.querySelector(".SpecialDefense1");
+  let p2SpecialDefense = document.querySelector(".SpecialDefense2");
+
+  if (p1[0].SpecialDefense > p2[0].SpecialDefense) {
+    p1SpecialDefense.classList.add("greenProgress");
+    p2SpecialDefense.classList.add("redProgress");
+    statCountP1++;
+  } else if (p1[0].SpecialDefense < p2[0].SpecialDefense) {
+    p1SpecialDefense.classList.add("redProgress");
+    p2SpecialDefense.classList.add("greenProgress");
+    statCountP2++;
+  } else {
+    p1SpecialDefense.classList.add("orangeProgress");
+    p2SpecialDefense.classList.add("orangeProgress");
+  }
+
+  let p1Speed = document.querySelector(".Speed1");
+  let p2Speed = document.querySelector(".Speed2");
+
+  if (p1[0].Speed > p2[0].Speed) {
+    p1Speed.classList.add("greenProgress");
+    p2Speed.classList.add("redProgress");
+    statCountP1++;
+  } else if (p1[0].Speed < p2[0].Speed) {
+    p1Speed.classList.add("redProgress");
+    p2Speed.classList.add("greenProgress");
+    statCountP2++;
+  } else {
+    p1Speed.classList.add("orangeProgress");
+    p2Speed.classList.add("orangeProgress");
+  }
+
+  if (statCountP1 > statCountP2) {
+    let h3 = document.createElement("h3");
+    h3.textContent = "Pokemon1 has better stats than pokemon2";
+    battleField.append(h3);
+  } else if (statCountP1 < statCountP2) {
+    let h3 = document.createElement("h3");
+    h3.textContent = "Pokemon2 has better stats than pokemon1";
+    battleField.append(h3);
+  } else {
+    let h3 = document.createElement("h3");
+    h3.textContent = "These Pokémons are evenly matched";
+    battleField.append(h3);
+  }
+
+  console.log("P1 stat count: ", statCountP1);
+  console.log("P2 stat count: ", statCountP2);
+
+  for (let stat in stats1) {
+    if (stats1[stat] > stats2[stat]) {
+      console.log(
+        `${p1.name} has higher ${stat} (${stats1[stat]} vs ${stats2[stat]})`
+      );
+    } else if (stats1[stat] < stats2[stat]) {
+      console.log(
+        `${p2.name} has higher ${stat} (${stats2[stat]} vs ${stats1[stat]})`
+      );
+    } else {
+      console.log(`Both have the same ${stat} (${stats1[stat]})`);
+    }
+  }
+
+  // for (let stat in stats1) {
+  //   if (stats1[stat] > stats2[stat]) {
+  //     result += `${pokemon1.name} has higher ${stat} (${stats1[stat]} vs ${stats2[stat]})<br>`;
+  //   } else if (stats1[stat] < stats2[stat]) {
+  //     result += `${pokemon2.name} has higher ${stat} (${stats2[stat]} vs ${stats1[stat]})<br>`;
+  //   } else {
+  //     result += `Both have the same ${stat} (${stats1[stat]})<br>`;
+  //   }
+  // }
+
+  // Assuming you have a div with id 'result' in your HTML
+  // document.querySelector(".result").innerHTML = result;
+
+  fight(p1, p2);
+}
+
+function fight() {
+  console.log(p1);
+  console.log(p2);
+  // let button = document.createElement("button");
+  // button.innerText = "FIGHT";
+  // battleField.append(button);
+}
